644c064ecbe4509348be21aeebb89e2f0b47e6cd
ryantanner <ryan.tanner@gmail.com>
2014-05-21 16:45:46 -0600

    way simpler

---
+++ Processing.scala
@@ -0,0 +1,110 @@
+package processing
+
+import scala.concurrent._
+
+sealed trait Input[+I]
+
+object Input {
+  case class Element[+I](elem: I) extends Input[I]
+  case object Empty extends Input[Nothing]
+  case object End extends Input[Nothing]
+}
+
+/**
+ * Represents the current state of a consumer
+ */
+sealed trait State[S, +I]
+
+object State {
+  case class Continue[S, I](process: Input[I] => Consumer[S, I]) extends State[S, I]
+  case class Done[S, +I](finalState: S) extends State[S, I]
+  case class Error[S](ex: Throwable) extends State[S, Nothing]
+
+  /*
+  def apply[I, S](state: S, input: Input[I] = Input.Empty): Consumer[S, I] = state match {
+    case State.Done(state) => new StatefulConsumer[S, I] { def fold[B]() }
+  }
+  */
+}
+
+class Consumer[S, I](state: State[S, I])/*(fn: (S, I) => Consumer[S, I])*/ {
+
+  def feed(input: I): Consumer[S, I] = state match {
+    case State.Continue(process) => process(Input.Element(input))
+    case State.Done(finalState)  => new Consumer(State.Done(finalState))
+    case s: State.Error[S]       => new Consumer(s)
+  }
+
+  def feedAll(inputs: TraversableOnce[I]): Consumer[S, I] = inputs.foldLeft(this) { (acc, i) =>
+    acc.feed(i)
+  }
+
+  def eof: Consumer[S, I] = state match {
+    case State.Continue(process) => process(Input.End)
+    case State.Done(finalState)  => new Consumer(State.Done(finalState))
+    case s: State.Error[S]       => new Consumer(s)
+  }
+
+  def run: S = state match {
+    case State.Done(finalState) => finalState
+    case State.Error(ex)        => throw ex
+    case _: State.Continue => sys.error("consumer not done")
+  }
+
+}
+
+class AsyncConsumer[S, I](state: Future[State[S, I]])/*(fn: (S, I) => Consumer[S, I])*/ {
+
+  def feed(input: Future[I]): AsyncConsumer[S, I] = state.collect {
+    case State.Done(finalState)  => new Consumer(State.Done(finalState))
+    case s: State.Error[S]       => new Consumer(s)
+    case State.Continue(process) => process(Input.Element(input))
+      val c = promise[State[S, I]]
+
+      input.onComplete {
+        case Success(i) => c.success(process(Input.Element(i)))
+        case Failure(ex) => c.success(Input.Error(ex))
+      }
+
+      new AsyncConsumer(c.future)
+  }
+
+  def feedAll(inputs: TraversableOnce[I]): Consumer[S, I] = inputs.foldLeft(this) { (acc, i) =>
+    acc.feed(i)
+  }
+
+  def eof: Consumer[S, I] = state match {
+    case State.Continue(process) => process(Input.End)
+    case State.Done(finalState)  => new Consumer(State.Done(finalState))
+    case s: State.Error[S]       => new Consumer(s)
+  }
+
+  def run: S = state match {
+    case State.Done(finalState) => finalState
+    case State.Error(ex)        => throw ex
+    case _: State.Continue => sys.error("consumer not done")
+  }
+
+}
+
+object Consumer {
+
+  //def fold[S, I](fn: (S, I) => S): Consumer[S, I] = fold(State.Continue)
+
+  /**
+   * Creates a stateful consumer which folds over its input using
+   * the given function and initial state
+   */
+  def fold[S, I](state: S)(fn: (S, I) => S): Consumer[S, I] = {
+
+    def step(state: S)(input: Input[I]): Consumer[S, I] = input match {
+      case Input.Element(elem) => new Consumer(State.Continue(step(fn(state, elem))))
+      case Input.Empty         => new Consumer(State.Continue(step(state)))
+      case Input.End           => new Consumer(State.Done(state))
+    }
+
+    new Consumer(State.Continue[S, I](step(state)))
+
+  }
+
+}
--- Processor.scala
+++ Processor.scala
@@ -1,5 +1,4 @@
-package processors
-
+/*
 sealed trait Input[+I]
 
 object Input {
@@ -7,37 +6,50 @@ object Input {
   case object Empty extends Input[Nothing]
   case object End extends Input[Nothing]
 }
-
+*/
+/**
+ * Represents the current state of a consumer
+ */ /*
 sealed trait State[S, +I]
 
 object State {
-  case class Continue[S, +I](process: Input[I] => Processor[S, I]) extends State[S, I]
+  case class Continue[S, +I](process: Input[I] => Consumer[S, I]) extends State[S, I]
   case class Done[S, +I](finalState: S) extends State[S, I]
-  case object Error[I] extends State[Nothing, I]
-}
+  case class Error[S](ex: Throwable) extends State[S, Nothing]
 
-trait Processor[+S, I] {
-
-  // A purely side-effecting processor
-  def process(input: Input[I]): Unit = input match {
-    case Element(elem) => println(elem)
-    case Empty         => println("Empty!")
-    case End           => println("End!")
+  def apply[I, S](state: S, input: Input[I] = Input.Empty): Consumer[S, I] = state match {
+    case State.Done(state) => new StatefulConsumer[S, I] { def fold[B]() }
   }
+}
+
+trait Consumer[+S, I] {
+
+  def fold[B](folder: State[S, I] => B): B
+
+  def run: S = fold({
+    case State.Done(finalState)   => finalState
+    case State.Error(ex)          => throw ex
+    case State.Continue(process)  => process(Input.End).fold({
+      case State.Done(finalState) => finalState
+      case State.Continue(_)      => throw new Exception("Consumer not done after input ended")
+      case State.Error(ex)        => throw ex
+    })
+  })
 
 }
 
-// I could implement this as an actor
-object Processor {
+object Consumer {
+*/
+  /**
+   * Creates a stateful consumer which folds over its input using
+   * the given function and initial state
+   */ /*
+  def fold[S, I](state: S)(fn: (S, I) => S): Consumer[S, I] = {
 
-  // We could also have a stateful processsor
-  // Note the recursion!
-  def fold[S, I](state: S)(fn: (S, I) => S): Processor[S, I] =
-
-    def step(state: S)(input: Input[I]): Processor[S, I] = input match {
-      case Element(elem) => State.Continue(step(fn(state, elem)))
-      case Empty         => State.Continue(step(state))
-      case End           => State.Done(state)
+    def step(state: S)(input: Input[I]): Consumer[S, I] = input match {
+      case Input.Element(elem) => State.Continue(step(fn(state, elem)))
+      case Input.Empty         => State.Continue(step(state))
+      case Input.End           => State.Done(state)
     }
 
     State.Continue[S, I](step(state))
@@ -45,3 +57,12 @@ object Processor {
   }
 
 }
+
+// A stateful consumer is its own state
+trait StatefulConsumer[S, I] extends Consumer[S, I] with State[S, I] {
+
+  def fold[B](folder: State[S, I] => B): B = {
+    folder(this)
+  }
+
+}*/
